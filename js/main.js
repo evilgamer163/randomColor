6fd8dfe5bd4715eb6bb6bff5d819b7fc6c1d8aef
evilgamer163 <evilgamestm@gmail.com>
2020-06-23 16:01:34 +0400

    Правки:)

--- js/main.js
+++ js/main.js
@@ -4,16 +4,14 @@ const body = document.querySelector('body'),
     colorTitle = document.querySelector('.color-title'),
     colorBtn = document.querySelector('.color-btn');
 
-const randomNum = (max, min) => {
-    max = Math.floor(max);
-    min = Math.ceil(min);
-    return (Math.floor(Math.random() * (max-min)) + min).toString(16);
+const randomNum = () => {
+    return (Math.floor(Math.random() * 255).toString(16));
 };
 
 const randColor = () => {
     let randomColor = '';
     for(let i = 0; i < 3; i++) {
-        randomColor += randomNum(255, 0);
+        randomColor += randomNum();
     }
     colorTitle.textContent = `#${randomColor}`;
     body.style.backgroundColor = `#${randomColor}`;
